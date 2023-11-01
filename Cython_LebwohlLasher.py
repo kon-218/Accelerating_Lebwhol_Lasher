5fa4529501b5a65db70425e6b6e9eac53567437f
Konstantin Nomerotski <kon50@hotmail.co.uk>
2023-11-01 12:49:33 +0000

    Cythonized with cpython variable definitions, setup and run file. main function moved to cython

--- Cython_LebwohlLasher.py
+++ Cython_LebwohlLasher.py
@@ -23,68 +23,22 @@ SH 16-Oct-23
 """
 
 import sys
-import time
-import datetime
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-from Cython.CythonLebwohlLasher import one_energy, all_energy, savedat, initdat, get_order, plotdat, MC_step
+import time 
 
+#import Cython.CythonLebwohlLasher as cythonLL
+from cythonized.cythonized.CythonLebwohlLasher import main
 
-
-
-def main(program, nsteps, nmax, temp, pflag):
-    """
-    Arguments:
-	  program (string) = the name of the program;
-	  nsteps (int) = number of Monte Carlo steps (MCS) to perform;
-      nmax (int) = side length of square lattice to simulate;
-	  temp (float) = reduced temperature (range 0 to 2);
-	  pflag (int) = a flag to control plotting.
-    Description:
-      This is the main function running the Lebwohl-Lasher simulation.
-    Returns:
-      NULL
-    """
-    # Create and initialise lattice
-    lattice = initdat(nmax)
-    # Plot initial frame of lattice
-    plotdat(lattice,pflag,nmax)
-    # Create arrays to store energy, acceptance ratio and order parameter
-    energy = np.zeros(nsteps+1,dtype=np.dtype)
-    ratio = np.zeros(nsteps+1,dtype=np.dtype)
-    order = np.zeros(nsteps+1,dtype=np.dtype)
-    # Set initial values in arrays
-    energy[0] = all_energy(lattice,nmax)
-    ratio[0] = 0.5 # ideal value
-    order[0] = get_order(lattice,nmax)
-
-    # Begin doing and timing some MC steps.
+if int(len(sys.argv)) == 5:
+    PROGNAME = sys.argv[0]
+    ITERATIONS = int(sys.argv[1])
+    SIZE = int(sys.argv[2])
+    TEMPERATURE = float(sys.argv[3])
+    PLOTFLAG = int(sys.argv[4])
     initial = time.time()
-    for it in range(1,nsteps+1):
-        ratio[it] = MC_step(lattice,temp,nmax)
-        energy[it] = all_energy(lattice,nmax)
-        order[it] = get_order(lattice,nmax)
+    main(PROGNAME,ITERATIONS,SIZE,TEMPERATURE,PLOTFLAG)
     final = time.time()
-    runtime = final-initial
-    
-    # Final outputs
-    print("{}: Size: {:d}, Steps: {:d}, T*: {:5.3f}: Order: {:5.3f}, Time: {:8.6f} s".format(program, nmax,nsteps,temp,order[nsteps-1],runtime))
-    # Plot final frame of lattice and generate output file
-    savedat(lattice,nsteps,temp,runtime,ratio,energy,order,nmax)
-    plotdat(lattice,pflag,nmax)
-#=======================================================================
-# Main part of program, getting command line arguments and calling
-# main simulation function.
-#
-if __name__ == '__main__':
-    if int(len(sys.argv)) == 5:
-        PROGNAME = sys.argv[0]
-        ITERATIONS = int(sys.argv[1])
-        SIZE = int(sys.argv[2])
-        TEMPERATURE = float(sys.argv[3])
-        PLOTFLAG = int(sys.argv[4])
-        main(PROGNAME, ITERATIONS, SIZE, TEMPERATURE, PLOTFLAG)
-    else:
-        print("Usage: python {} <ITERATIONS> <SIZE> <TEMPERATURE> <PLOTFLAG>".format(sys.argv[0]))
+    time_taken = final - initial
+    print(f"Time taken: {time_taken:.5f}s")
+else:
+    print("Usage: python {} <ITERATIONS> <SIZE> <TEMPERATURE> <PLOTFLAG>".format(sys.argv[0]))
 #=======================================================================
